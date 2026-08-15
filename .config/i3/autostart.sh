9361dd48673835d6013c58c7b9f82a78099223c8
mawpz <a.taylor.4086@gmail.com>
2026-08-15 02:41:37 +0100

    [i3 autostart] - Change wallpaper

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-xset r rate 300 50
-setxkbmap gb
+# xset r rate 300 50
+# setxkbmap gb
 unclutter --start-hidden --timeout 3 -b
 
 ## DISPLAY SETTINGS ##
@@ -9,9 +9,9 @@ xrandr --output DP-2 --mode 1920x1080 -r 100 --primary --right-of HDMI-0
 xrandr --output HDMI-0 --mode 1920x1080 -r 100
 
 setsid dunst &
-setsid picom -fc &
+setsid picom -fc --blur-method gaussian --blur-deviation 25 &
 
-xwallpaper --zoom /mnt/5tb/Pictures/walls/BB1msMCi.jpg
+xwallpaper --zoom $HOME/.config/PXL_20260814_210924196.NIGHT.jpg
 
 IsNumLock="$(cat "/sys/class/leds/input2::numlock/brightness")"
 [[ "$IsNumLock" == "0" ]] && xdotool key Num_Lock
